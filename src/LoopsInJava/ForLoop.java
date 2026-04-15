483cfe65a1788f843a92a905697d7bb0fa3b4dbd
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-04-15 13:35:09 +0530

    Loops in java

---
+++ src/LoopsInJava/ForLoop.java
@@ -0,0 +1,15 @@
+package LoopsInJava;
+
+public class ForLoop {
+    static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println("Have a great life ahead");
+            System.out.println(i);
+        }
+        int j;
+        for(j=1;j<=8;j++){
+            System.out.print(j+" ");
+        }
+        System.out.println(j);
+    }
+}
