2cecc2dcc0295ae67d0e1c6118485ffc5955660f
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-05-08 15:24:41 +0530

    Loops in java Arithmetic and Geometric Progression prime number ascii values

---
+++ src/LoopsInJava/While.java
@@ -0,0 +1,11 @@
+package LoopsInJava;
+
+public class While {
+    static void main(String[] args) {
+        int i = 0;
+        for (;i <=10;) {
+            System.out.println("Hello Developers!!");
+            i++;
+        }
+    }
+}
