20697488b1f9f3fc42b3656c2699c7cb6c548c77
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-06-18 20:32:33 +0530

    Patterns Printing Square ,Numbers , Alphabets pattern printing

--- src/Patterns/GivenAlphabetsPattern.java
+++ src/Patterns/GivenAlphabetsPattern.java
@@ -2,14 +2,14 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class SmallAlphabets {
+public class GivenAlphabetsPattern {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the numbers : ");
+        System.out.print("Enter the numbers: ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n ; i++) {
-            for (int j = 1; j <=n; j++) {
-                System.out.print((char)(j+96) + " ");
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n ; j++) {
+                System.out.print((char)(i+64) +" ");
             }
             System.out.println();
         }
--- src/Patterns/QuestionofAlphabet.java
+++ src/Patterns/QuestionofAlphabet.java
@@ -2,15 +2,17 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class SmallAlphabets {
+public class QuestionofAlphabet {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the numbers : ");
+        System.out.print("Enter the number :");
         int n = sc.nextInt();
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <=n; j++) {
-                System.out.print((char)(j+96) + " ");
+                System.out.print((char)(i+96+1)+" ");
+                System.out.println((char)(i+66+1)+" ");
             }
+
             System.out.println();
         }
     }
