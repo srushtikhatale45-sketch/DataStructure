82c2ddd56348dca3bf075f59b4a581ddfcde0927
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-06-23 10:39:10 +0530

    Patterns Printing Flipped Traingle,Reverse Traingle ,Numbers , Alphabets pattern printing

---
+++ src/Patterns/FlippedTriangle.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class FlippedTriangle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter  the number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                if(i+j>n){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
