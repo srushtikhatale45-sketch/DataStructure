8f30eadd3eee1bc3a822ab74b65312f80b03bdc0
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-06-24 14:37:59 +0530

    Patterns Printing Flipped Traingle,Reverse Traingle ,Numbers , Alphabets,Rhombus,Pyramid pattern printing

--- src/Patterns/DiamondStars.java
+++ src/Patterns/DiamondStars.java
@@ -1,5 +1,40 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class DiamondStars {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        int nsp = n-1;
+        int nst = 1;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            nsp--;
+            nst+=2;
+        }
+
+        nsp =1;
+        nst = 2*n-3;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            nsp+=1;
+            nst-=2;
+        }
+
+    }
 
 }
