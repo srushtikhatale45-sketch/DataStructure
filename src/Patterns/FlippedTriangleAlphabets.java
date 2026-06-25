ad4193ca6f903bf1a58a9d069d74c74cd3f7295c
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-06-25 22:43:16 +0530

    Patterns Printing Flipped Traingle,Reverse Traingle ,Numbers , Alphabets,Rhombus,Pyramid pattern printing

---
+++ src/Patterns/FlippedTriangleAlphabets.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class FlippedTriangleAlphabets {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n+1-i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <=i; j++) {
+               System.out.print((char)(i+96)+" "); //small case alphabets
+            }
+            System.out.println();
+        }
+    }
+}
