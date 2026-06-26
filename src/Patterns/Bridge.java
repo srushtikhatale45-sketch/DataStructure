2bad58358e9f2b728f8745115021fb146dbac685
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-06-26 11:54:37 +0530

    Patterns Printing Flipped Traingle,Reverse Traingle ,Numbers , Alphabets,Rhombus,Pyramid pattern printing,Bridge,/number Spiral

---
+++ src/Patterns/Bridge.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Bridge {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number here: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 2*n-1 ; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        for (int i = 1; i <= n-1 ; i++) {
+            for (int j = 1; j <= n-i ; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <=2*i-1; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= n-i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
