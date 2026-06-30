b8f42dd5b8736c017332722e556dfb89e4dd2be0
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-06-30 22:11:47 +0530

    Patterns Printing Flipped Traingle,Reverse Traingle ,Numbers , Alphabets,Rhombus,Pyramid pattern printing,Bridge,/number Spiral

--- src/Patterns/NumberSpiral.java
+++ src/Patterns/NumberSpiral.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class NumberSpiral {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Printing Number Spiral");
         System.out.print("Enter the number: ");
         int n= sc.nextInt();
         for (int i = 1; i <=2*n-1; i++) {
