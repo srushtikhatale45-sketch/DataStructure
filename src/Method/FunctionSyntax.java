df8640b5332404c92f94b1b3a4d011aa177b3adb
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-02 10:36:48 +0530

    Functions in java

---
+++ src/Method/FunctionSyntax.java
@@ -0,0 +1,17 @@
+package Method;
+
+public class FunctionSyntax {
+//    we can write function here
+    public static void intro(){
+        System.out.println("Learning a Functions in java!!");
+    }
+    static void main(String[] args) {
+        System.out.println("I am a main method in java");
+        intro();//calling function intro
+        basics();//calling function basics
+    }
+    public static void basics(){
+        System.out.println("I am a main method in java");
+    }
+    //   we can write function here
+}
