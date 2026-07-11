b7c3842018715b4673017782e5916f15e35ff932
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-11 21:33:17 +0530

    Shallo copy of array  and the datatypes of the array,array references

--- src/Array/Shallowcopy.java
+++ src/Array/Shallowcopy.java
@@ -5,6 +5,8 @@ public class Shallowcopy {
         int[] arr ={1,2,3,4,5,6,7,8,9,10};
         int[] arr1 = arr;
         arr1[2]=6;
+        int[] arr2 = arr1;
+        arr2[2]=7;
         System.out.println(arr[2]);
     }
 }
