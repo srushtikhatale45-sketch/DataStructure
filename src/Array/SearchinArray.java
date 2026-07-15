eaaea89a107fbeef3cecac8048b3d74d5f9fbe71
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-15 14:45:07 +0530

    search element in array,sortelement in array

--- src/Array/SearchinArray.java
+++ src/Array/SearchinArray.java
@@ -1,19 +1,31 @@
 package Array;
 
+import java.util.Scanner;
+
 public class SearchinArray {
     static void main(String[] args) {
-        int[] arr = {10,20,30,40,50};
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the number of element present in the array:  ");
+        int n= sc.nextInt();
+        int[] arr = new int[n];
         boolean found = false;
-        int key=30;
+        System.out.print("Enter the  element present in the array:  ");
+        for (int i = 0; i < n ; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("enter the target value to be found: ");
+        int key=sc.nextInt();
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]==key);
-            found=true;
+            if(arr[i]==key) {
+                System.out.print(i);
+                found = true;
+                break;
+            }
         }
         if(found){
             System.out.println("Element is present.");
         }
         else {
-            System.out.println("Element is not present.");
             System.out.println("Element is not present.");
         }
     }
