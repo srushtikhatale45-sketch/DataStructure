c8846dd102e626ef610db9913f407fd8e0ccd418
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-15 15:06:12 +0530

    Second Max Element in the array

---
+++ src/Array/SecondLargestnum.java
@@ -0,0 +1,53 @@
+package Array;
+
+import java.util.Scanner;
+
+public class SecondLargestnum {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements present in array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        print(arr);
+
+        int secondLargest = getSecondLargest(arr);
+
+        System.out.println("\nSecond Largest Element: " + secondLargest);
+
+        sc.close();
+    }
+
+    public static int getSecondLargest(int[] arr) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        return secondLargest;
+    }
+
+    public static void print(int[] arr) {
+        System.out.print("Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
