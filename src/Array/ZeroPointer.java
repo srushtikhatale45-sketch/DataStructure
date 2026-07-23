b6c4bff3ebb1af29ae0e698e6f810922f44658ea
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-23 22:26:30 +0530

    Segregating Zeros and Ones by using two pointer

--- src/Array/ZeroPointer.java
+++ src/Array/ZeroPointer.java
@@ -7,6 +7,7 @@ public class ZeroPointer {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the Array: ");
         int n = sc.nextInt();
+        System.out.print("Enter the array Element: ");
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i]= sc.nextInt();
